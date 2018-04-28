83e1e0f8e0195fee6a0d321d08b9e56852a517d2
Rin <30601759+Darksamuraj@users.noreply.github.com>
2018-04-28 19:45:37 +0300

    get some changes

--- src/testcalculate/Testcalculate.java
+++ src/testcalculate/Testcalculate.java
@@ -7,8 +7,8 @@ public class Testcalculate {
  
     public static void main(String[] args) {
         // TODO code application logic here
-        int a=2,b=3;
-        System.out.println(a+b);
+        int a=2,b=3,c=4;
+        System.out.println(a+b+c);
     }
     
 }

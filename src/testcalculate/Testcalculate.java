3e9d87280c560c3f92dcd2147c60862caf2b40eb
Rin <30601759+Darksamuraj@users.noreply.github.com>
2018-04-28 18:31:50 +0300

    Добавил выичление простое

---
+++ src/testcalculate/Testcalculate.java
@@ -0,0 +1,14 @@
+
+package testcalculate;
+
+
+public class Testcalculate {
+
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=2,b=3;
+        System.out.println(a+b);
+    }
+    
+}
